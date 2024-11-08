0105ce169d4c97c6ad862df6eb736f6231f6f036
Eduardo <dudugamer929@gmail.com>
2024-11-08 10:13:08 -0400

    BREAKING CHANGE: alterando o Header

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,48 +1,85 @@
-import React, { useState, useEffect } from 'react';
-import MyLogo from '../../assets/img/mylogo.png';
-import { Navbar, Nav, Container } from 'react-bootstrap';
-import './header.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React, { useState, useEffect } from 'react'
+import { Navbar, Nav, Container } from 'react-bootstrap'
+import './header.css'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 export const Header = () => {
-   const [activeLink, setActiveLink] = useState('home');
-   const [scrolled, setScrolled] = useState(false);
+  const [activeLink, setActiveLink] = useState('home')
+  const [scrolled, setScrolled] = useState(false)
 
-   useEffect(() => {
+  useEffect(() => {
     const onScroll = () => {
-        if (window.screenY > 50){
-            setScrolled(true);
-        } else {
-            setScrolled(false);
-        }
+      if (window.screenY > 50) {
+        setScrolled(true)
+      } else {
+        setScrolled(false)
+      }
     }
 
-    window.addEventListener("scroll", onScroll);
+    window.addEventListener('scroll', onScroll)
 
-    return () => window.removeEventListener("scroll", onScroll);
-   }, []);
+    return () => window.removeEventListener('scroll', onScroll)
+  }, [])
 
-   const linkActive = (value) => {
-    setActiveLink(value);
-   }
+  const linkActive = (value) => {
+    setActiveLink(value)
+  }
 
-   return(
+  return (
     <Router>
-        <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
-            <Container>
-                <Navbar.Brand href="/">
-                <img src={MyLogo} alt="Logo"/>
-                </Navbar.Brand>
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className='ms-auto'>
-                        <Nav.Link href="#products" className={activeLink === 'products' ? 'active navbar-link' : 'navbar-link'} onClick={() => linkActive('products')}>Nossos Produtos</Nav.Link>
-                        <Nav.Link href="#beneficies" className={activeLink === 'beneficies' ? 'active navbar-link' : 'navbar-link'} onClick={() => linkActive('beneficies')}>Beneficios</Nav.Link>
-                        <Nav.Link href="#depoiments" className={activeLink === 'depoiments' ? 'active navbar-link' : 'navbar-link'} onClick={() => linkActive('depoiments')}>Depoimentos</Nav.Link>
-                        <Nav.Link href="#orcamento" className={activeLink === 'orcamento' ? 'active navbar-link' : 'navbar-link'} onClick={() => linkActive('orcamento')}>Solicitar Orçamento</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+      <Navbar expand='md' className={scrolled ? 'scrolled' : ''}>
+        <Container>
+          <Navbar.Brand href='/'></Navbar.Brand>
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ms-auto'>
+              <Nav.Link
+                href='#products'
+                className={
+                  activeLink === 'products'
+                    ? 'active navbar-link'
+                    : 'navbar-link'
+                }
+                onClick={() => linkActive('products')}
+              >
+                Nossos Produtos
+              </Nav.Link>
+              <Nav.Link
+                href='#beneficies'
+                className={
+                  activeLink === 'beneficies'
+                    ? 'active navbar-link'
+                    : 'navbar-link'
+                }
+                onClick={() => linkActive('beneficies')}
+              >
+                Beneficios
+              </Nav.Link>
+              <Nav.Link
+                href='#depoiments'
+                className={
+                  activeLink === 'depoiments'
+                    ? 'active navbar-link'
+                    : 'navbar-link'
+                }
+                onClick={() => linkActive('depoiments')}
+              >
+                Depoimentos
+              </Nav.Link>
+              <Nav.Link
+                href='#orcamento'
+                className={
+                  activeLink === 'orcamento'
+                    ? 'active navbar-link'
+                    : 'navbar-link'
+                }
+                onClick={() => linkActive('orcamento')}
+              >
+                Solicitar Orçamento
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </Router>
-   )
+  )
 }
--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,10 +1,11 @@
 import { Header } from "../../components/Header/header";
+import { Pagina } from "../Page 1/page1";
 
 export default function Home(){
     return(
         <>
         <Header/>
-        <firstPage/>
+        <Pagina/>
         </>
     )
 }
