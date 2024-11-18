99dd7d54de40529b0999f4d4163cfd1726f5f991
Eduardo <dudugamer929@gmail.com>
2024-11-17 22:43:11 -0400

    feat: add home Pagina5

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -3,6 +3,7 @@ import { Pagina } from '../Page 1/page1'
 import { Pagina2 } from '../Page 2/page2'
 import { Pagina3 } from '../Page 3/page3'
 import { Pagina4 } from '../Page 4/page4'
+import { Pagina5 } from '../Page 5/page5'
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <Pagina2 />
       <Pagina3 />
       <Pagina4 />
+      <Pagina5 />
     </>
   )
 }
---
+++ src/pages/Page 5/page5.js
@@ -0,0 +1,9 @@
+import './page5.css'
+
+export const Pagina5 = () => {
+  return (
+    <div className='container5'>
+      <div className='quader-red'></div>
+    </div>
+  )
+}
